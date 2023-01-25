b7c7c9782a76ef7a157cb86aabec785d91868baf
KimHaechan <ckssmsla98@naver.com>
2023-01-25 16:30:17 +0900

    del, add ui

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -1,20 +1,24 @@
-import classNames from 'classnames';
 import React from 'react';
 import { MdDone, MdDelete } from 'react-icons/md';
 import './css/TodoItem.css';
 import cn from 'classnames';
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo, remove }) => {
 
-  const {title, done} = todo;  
+  const {id, title, done} = todo;  
+
+  // 서버에 삭제요청 클릭 이벤트핸들러
+  const deleteClickHandler = e => {
+    remove(id);
+  };
 
   return (
     <li className="todo-item">
-        <div className={cn("check-circle",{active:done})}>
+        <div className={cn('check-circle', {active: done})}>
             {done && <MdDone />}
         </div>
-        <span className={cn("text",{finish:done})} >{title}</span>
-        <div className="remove">
+        <span className={cn('text', {finish: done})}>{title}</span>
+        <div className="remove" onClick={deleteClickHandler}>
             <MdDelete />
         </div>
     </li>
